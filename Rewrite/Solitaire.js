112bd2c6a194b50d4d67793f485239123d73d733
MasterHoo <164809594+MasterHoo@users.noreply.github.com>
2026-06-15 19:52:06 +0800

    Delete Rewrite/Solitaire.js

--- Rewrite/Solitaire.js
+++
@@ -1,9 +0,0 @@
-[rewrite_local]
-# 强制让广告请求返回空数据，直接骗过游戏跳过弹窗
-^https?:\/\/.*\.applovin\.com\/.*$ url reject-200
-^https?:\/\/.*\.applvn\.com\/.*$ url reject-200
-^https?:\/\/.*\.fyber\.com\/.*$ url reject-200
-
-[mitm]
-# 必须配置主机名解密，重写才会生效
-hostname = *.applovin.com, *.applvn.com, *.fyber.com
